accb4a5accbc4091237b749679c91c0db20408e1
Agnieszka Chalat <kontakt@akramarz.de>
2022-04-02 19:56:32 +0200

    Order Endpoint URL changed

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/orders")
+@RequestMapping("v1/orders")
 public class OrderController {
 
     @GetMapping
