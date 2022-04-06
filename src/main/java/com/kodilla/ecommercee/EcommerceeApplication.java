623c159d06950a0404fd1145d4ef87a21c4f5c5e
jakubpanienski@wp.pl <jakubpanienski@wp.pl>
2022-04-06 18:17:37 +0200

    update

--- src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
+++ src/main/java/com/kodilla/ecommercee/EcommerceeApplication.java
@@ -1,6 +1,6 @@
 package com.kodilla.ecommercee;
 
-import com.kodilla.ecommercee.dto.GroupsDto;
+import com.kodilla.ecommercee.dto.GroupDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/dto/GroupDto.java
@@ -4,12 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class GroupsDto {
+public class GroupDto {
     private Long id;
     private String groupName;
     // private ArrayList<Products>;
