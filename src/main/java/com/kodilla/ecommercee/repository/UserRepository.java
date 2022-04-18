a6820f417525f5984e0aa3782ee0e02167a603a6
Joel Chawraj <67972266+dzejsiejcz@users.noreply.github.com>
2022-04-18 09:08:32 +0200

    testy-poprawione po review Agi i Grzegorza

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -9,17 +9,5 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<User, Long> {
-
-    @Override
-    List<User> findAll();
-
-    @Override
-    User save(User user);
-
-    @Override
-    Optional<User> findById(Long id);
-
-    @Override
-    void deleteById(Long id);
 }
 
