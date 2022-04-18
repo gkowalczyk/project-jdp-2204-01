db12a919f8d442d45fa2b629cdec652ec7b9af2b
rafalcisz1991 <rafal.cisz1991@gmail.com>
2022-04-18 09:28:31 +0200

    commit before mergin to main

--- src/main/java/com/kodilla/ecommercee/domain/Cart.java
+++ src/main/java/com/kodilla/ecommercee/domain/Cart.java
@@ -22,8 +22,8 @@ public class Cart {
     @ManyToMany(cascade = CascadeType.ALL)
     @JoinTable(
             name = "JOIN_PRODUCT_CART",
-            joinColumns = {@JoinColumn(name = "CART_ID", referencedColumnName = "CART_ID")},
-            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID", referencedColumnName = "PRODUCT_ID")}
+            joinColumns = {@JoinColumn(name = "CART_ID")},
+            inverseJoinColumns = {@JoinColumn(name = "PRODUCT_ID")}
     )
     private List<Product> products = new ArrayList<>();
 }
